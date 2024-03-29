3a99199cdddb2e97eec2abfdd60451c185c25024
Nicholas Rose <ntrose90@gmail.com>
2024-03-29 08:26:00 -0700

    fetch lab

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json));
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+function renderBooks(booksData) {
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener("DOMContentLoaded", fetchBooks);
